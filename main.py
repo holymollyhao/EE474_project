d5825f64c754d135891f62d3152b38ab10dce838
Shinhyeok <shinhyuk1001@kaist.ac.kr>
2023-05-13 18:15:25 +0900

    feat: playlist response generation

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from utils import utils
 
 mood = "sentimental"
 genre = "jazz"
+hours = 1
 
 # for generating responses
 model = utils.set_openai()
-message = utils.construct_message_image(mood=mood, genre=genre)
-response = utils.generate_response(model=model, message=message)
-print(response)
+
+message_playlist =  utils.construct_message_playlist(hours=hours, mood=mood, genre=genre)
+response_playlist = utils.generate_response(model=model, message=message_playlist)
+
+message_image =     utils.construct_message_image(mood=mood, genre=genre)
+response_image =    utils.generate_response(model=model, message=message_image)
+
+
+print("Response of Playlist: ", response_playlist)
+print("Response of Image Prompt: ", response_image)
 
 # for image generation
 model = DreamLike()
-model.single_image_generation(response, mood=mood, genre=genre)
+model.single_image_generation(response_image, mood=mood, genre=genre)
