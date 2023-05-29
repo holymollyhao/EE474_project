f16ba74286d3ed99b656172ce9f1607cb98028c1
Max Kim <50355670+holymollyhao@users.noreply.github.com>
2023-05-29 21:53:21 +0900

    Revert "spotify audio features added"
    This reverts commit 1a78d931e4d31a272ab532448e364170edf9de13.

---
+++ vision_language_models/dreamlike.py
@@ -0,0 +1,19 @@
+from diffusers import StableDiffusionPipeline
+import torch
+import os
+
+
+class DreamLike:
+    def __init__(self, model_id="dreamlike-art/dreamlike-photoreal-2.0", result_path="./image_results"):
+        # model setup
+        self.model_id = model_id
+        self.result_path = result_path
+
+        # pipeline setup
+        # commandline_args = os.environ.get('COMMANDLINE_ARGS', "--skip-torch-cuda-test --no-half")
+        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
+        self.pipe.to("cuda")
+
+    def single_image_generation(self, prompt, mood, genre, date_time):
+        image = self.pipe(prompt, height=1080, width=1920).images[0]
+        image.save(f"{self.result_path}/mood-{mood}_genre-{genre}_{date_time}_result.jpg")
